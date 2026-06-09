edf9da7462572379721eecdb1ae29b3618266ea6
Inchara nk <incharank1@gmail.com>
2026-06-09 23:40:53 +0530

    header update

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>ToDo & Notes App</h1>
+      <p>Manage your tasks and notes efficiently</p>
+    </header>
+    
+  );
+}
+
+export default Header;
